eca72d710c5fa0cf1da05aa9c672b7d2fc9b297d
pray1n <89249367+pray1n@users.noreply.github.com>
2022-07-23 14:50:09 +0200

    test

--- my-app/src/NewsList.js
+++ my-app/src/NewsList.js
@@ -1,34 +1,35 @@
 import { useState, useEffect } from "react";
 
-function NewsList () {
-    const [newsListData, setNewsListData] = useState([])
-    useEffect(() => {getData()},[])
-      const getData = async () => {
-        const response = await fetch(`https://hn.algolia.com/api/v1/search?query=`)
-console.log(response)
-        if (response.ok) {
-          const result = await response.json();
-console.log(result)
-          setNewsListData(result)
-        }
-      }
-      
-
-    return (
-      <div>
-        <ul>
-      {newsListData.map((news)=>{
-        return(
-<li>news</li>
-        )
-      })}
-        <div>
-            
-            
-        </div>
-        </ul>
-        </div>
-        
-    )
+function NewsList() {
+  const [newsListData, setNewsListData] = useState([])
+  useEffect(() => { getData() }, [])
+  const getData = async () => {
+    const response = await fetch(`https://hn.algolia.com/api/v1/search?query=`)
+    console.log(response)
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result)
+      setNewsListData(result)
+    }
   }
+
+
+  return (
+    <div>
+      <ul>
+        {newsListData.map((news) => {
+          console.log(news)
+          return (
+            <li>{news.page}</li>
+       
+      )
+      
+      })}
+     
+      
+      </ul>
+    </div>
+
+  )
+}
 export default NewsList;
